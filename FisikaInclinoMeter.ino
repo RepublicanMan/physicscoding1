8e76cdc07d67ab61a8932d0e79e86fcb4b938114
Justin Jeremiah H <69155563+RepublicanMan@users.noreply.github.com>
2023-12-12 11:35:06 +0700

    Update FisikaInclinoMeter.ino

--- FisikaInclinoMeter.ino
+++ FisikaInclinoMeter.ino
@@ -4,16 +4,28 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL345_U.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
 
-Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345); // alamat I2C
+const int interruptPin = 2; // Pin to trigger the interrupt
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345); // alamat I2C integrated circuit communication
+
 // LCD pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 const int LiquidCrystal[] = {12,11,5,4,3,2};
 const int lengthPivotTuas = lengthPivotTuas;
-int AcceleroMeterValue = ;
+const int buttons = 1; // number of buttons
+const int buttonPins[buttons] = {2}; // Change pin numbers as per your connections
+const int buttonTimeout = 5000; // Time in milliseconds before automatic shutdown
+int AcceleroMeterValue;
+int d1;
+int d2;
+
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), wakeUp, LOW);
   lcd.begin(16,2);
   lcd.print("Initializing");
     if (!accel.begin()) {
@@ -29,8 +41,36 @@ void setup() {
   }
 
 }
-void loop() {
+  void autosleepproto() {
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Choose the desired sleep mode
+    sleep_enable();
+    sleep_mode();
+    stopActions();
+  }
+  void wakeUp() {
+    // This function is called when the interrupt pin triggers the interrupt
+    sleep_disable(); // Disable sleep mode
+    detachInterrupt(digitalPinToInterrupt(interruptPin)); // Detach the interrupt
+    startActions();
+  }
+
+  void loop() {
     // display enter input
+      lcd.print("Calibrate Sensor?")
+      while {
+        if (digitalRead(,))
+      }
+      for (int i = 0; i < buttons; i++) {
+      int buttonState = digitalRead(buttonPins[i]);
+      lastButtonPressTime = millis();
+       if(buttonState == HIGH){
+          lcd.print("waking up")
+          wakeUp();
+       }else{
+          delay(500);
+          lcd.print("sleep mode, press button to restart.")
+          autosleepproto();
+
   delay(500);
   lcd.print("Input jarak pivot pada ujung tuas berbeban"); 
     // masih memikir cara membuat sistem input data length	
@@ -39,8 +79,10 @@ void loop() {
   ADXLdata();
     // eksekusi kalkulasi dan memunculkan hasil
   d1d2Calc();
+    // hitung d1 dan d2
   lcd.print("Jarak antar benda dan tuas: "+d1+" dan panjang
   pegas adalah: "+d2+”);
+  //  hasil
 }
 
 void ADXLdata() {
@@ -53,7 +95,7 @@ void ADXLdata() {
   float accelerationZ = event.acceleration.z;
 
   float roll = atan2(accelerationY, accelerationZ) * 180.0 / PI;
-  float pitch = atan2(-accelerationX, sqrt(accelerationY * accelerationY + accelerationZ * accelerationZ)) * 180.0 / PI;
+  float pitch = atan2(-accelerationX, sqrt(accelerationY*accelerationY+accelerationZ*accelerationZ)) * 180.0 / PI;
 
   lcd.clear();
   // lcd.setCursor(0, 0);
@@ -74,6 +116,36 @@ void d1d2Calc() {
   int d2 = d1;
   return d1
   return d2 
+}
+
+void calibrateADXL345() {
+  Serial.println("Calibrating ADXL345. Please keep the sensor still...");
+  
+  float x_offset = 0.0, y_offset = 0.0, z_offset = 0.0;
+  const int num_samples = 100; // Number of samples for calibration
+  
+  for (int i = 0; i < num_samples; ++i) {
+    sensors_event_t event;
+    accel.getEvent(&event);
+    
+    x_offset += event.acceleration.x;
+    y_offset += event.acceleration.y;
+    z_offset += event.acceleration.z;
+    
+    delay(10);
+  }
+
+  x_offset /= num_samples;
+  y_offset /= num_samples;
+  z_offset /= num_samples;
+
+  Serial.println("Calibration complete.");
+  Serial.print("X-axis Offset: "); Serial.println(x_offset);
+  Serial.print("Y-axis Offset: "); Serial.println(y_offset);
+  Serial.print("Z-axis Offset: "); Serial.println(z_offset);
+  
+  // Apply offsets to the accelerometer (optional)
+  accel.setOffsets(x_offset, y_offset, z_offset);
 }
 
 
